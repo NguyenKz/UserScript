7d544554c02a15fdb150bccc74922379d5f26cb5
Trần Thảo Nguyên <48507110+NguyenKz@users.noreply.github.com>
2022-08-27 10:38:20 +0700

    test

--- truyenfull.js
+++ truyenfull.js
@@ -1,28 +1,19 @@
 // ==UserScript==
-// @name            TruyenCV downloader
-// @name:vi         TruyenCV downloader
-// @namespace       http://devs.forumvi.com/
-// @description     Tải truyện từ TruyenCV định dạng EPUB.
-// @description:vi  Tải truyện từ TruyenCV định dạng EPUB.
-// @version         4.6.8
-// @icon            http://i.imgur.com/o5cmtkU.png
-// @author          Zzbaivong
-// @oujs:author     baivong
-// @license         MIT; https://baivong.mit-license.org/license.txt
-// @match           http://truyencv.com/*/
-// @match           https://truyencv.com/*/
-// @require         https://code.jquery.com/jquery-3.5.1.min.js
+// @name         TruyenFullCreateEbook
+// @name:vi      TruyenFullCreateEbook
+// @namespace    http://nguyenkz.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       NguyenKz
+// @match        https://truyenfull.vn/*
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require http://code.jquery.com/jquery-3.4.1.min.js
 // @require         https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
 // @require         https://unpkg.com/file-saver@2.0.2/dist/FileSaver.min.js
-// @require         https://unpkg.com/ejs@2.7.4/ejs.min.js
-// @require         https://unpkg.com/jepub@2.1.4/dist/jepub.min.js
-// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
-// @noframes
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=userscripts-mirror.org
 // @connect         self
-// @supportURL      https://github.com/lelinhtinh/Userscript/issues
-// @run-at          document-idle
-// @grant           GM_xmlhttpRequest
-// @grant           GM.xmlHttpRequest
+// @grant   GM_getValue
+// @grant   GM_setValue
 // ==/UserScript==
 
 
