5040584c0781920315f65f57454ce4c732871265
Trần Thảo Nguyên <48507110+NguyenKz@users.noreply.github.com>
2022-12-31 23:51:48 +0700

    Update and rename GetLink.user.js to mneylinkpass.user.js

--- mneylinkpass.user.js
+++ mneylinkpass.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Link
+// @name         https://mneylinkpass.com/
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://mneylinkpass.com/*
+// @match        *
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mneylinkpass.com
 // @grant        none
 // ==/UserScript==
@@ -16,24 +16,24 @@
 
 function add_button(){
 
-	let element_parent = document.getElementsByTagName("body")[0];
-    let header = document.createElement("div");
+	let element_parent = document.body;
+    	let header = document.createElement("div");
 	header.setAttribute("id","nguyen_button")
 	element_parent.insertBefore(header, element_parent.firstChild);
 	element_parent = document.getElementById("nguyen_button")
 	element_parent.innerHTML = "<p></p>"
-    let button = document.createElement("button");
-    button.innerHTML = "<p>Page source</p>";
-    button.addEventListener ("click", function() {
-        let pattern = /el.html\(([\w]+)\)/i;
-        let result = pattern.exec(document.body.innerHTML);
-        let source = document.createElement("div");
-        source.innerHTML = "<p>"+result+"</p>"
-        element_parent.insertBefore(source, element_parent.firstChild);
-        console.log(source.innerHTML )
-    });
+	let button = document.createElement("button");
+	button.innerHTML = "<p>Page source</p>";
+	button.addEventListener ("click", function() {
+		let pattern = /el.html\(([\w]+)\)/i;
+		let result = pattern.exec(document.body.innerHTML);
+		let source = document.createElement("div");
+		source.innerHTML = "<p>"+result+"</p>"
+		element_parent.insertBefore(source, element_parent.firstChild);
+		console.log(source.innerHTML )
+	});
 
-    element_parent.insertBefore(button, element_parent.firstChild);
+	element_parent.insertBefore(button, element_parent.firstChild);
 
 }
 
