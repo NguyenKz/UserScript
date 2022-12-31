49fab4a1b438edda2c4e76d718af491ab5c5ff3d
Trần Thảo Nguyên <48507110+NguyenKz@users.noreply.github.com>
2022-12-31 12:20:00 +0700

    Update open_iframe.user.js

--- open_iframe.user.js
+++ open_iframe.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://*
+// @match        https://mneylink.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
