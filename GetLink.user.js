cdd88aaae121b007ae93a7329f46e12197ddbd5d
Trần Thảo Nguyên <48507110+NguyenKz@users.noreply.github.com>
2022-12-31 12:10:46 +0700

    Create GetLink.user.js

---
+++ GetLink.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Link
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://mneylinkpass.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mneylinkpass.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    add_button();
+})();
+
+
+function add_button(){
+
+	let element_parent = document.getElementsByTagName("body")[0];
+    let header = document.createElement("div");
+	header.setAttribute("id","nguyen_button")
+	element_parent.insertBefore(header, element_parent.firstChild);
+	element_parent = document.getElementById("nguyen_button")
+	element_parent.innerHTML = "<p></p>"
+    let button = document.createElement("button");
+    button.innerHTML = "<p>Page source</p>";
+    button.addEventListener ("click", function() {
+        let source = document.createElement("div");
+        source.innerHTML = "<p>"+(document.body.innerHTML+"").replaceAll("<","").replaceAll("/","").replaceAll(">","").replaceAll("\"","").replaceAll("'","")+"</p>"
+        element_parent.insertBefore(source, element_parent.firstChild);
+        console.log(source.innerHTML )
+    });
+
+    element_parent.insertBefore(button, element_parent.firstChild);
+
+}
+
+
+function _x(path){
+	var xpath = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	  var temp = [];
+	  for (var i = xpath.snapshotLength - 1; i >= 0; i--) {
+		temp.push(xpath.snapshotItem(i));
+	  }
+	  return temp;
+}
